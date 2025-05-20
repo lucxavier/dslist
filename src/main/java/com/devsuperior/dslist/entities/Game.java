df859e8cb56cd0c6607d4677bc42c4f4c6b8973c
lucas.henrique.xavier <lucas.h.xavier@gmail.com>
2025-05-20 09:45:01 -0300

    GameMinDTO, GameRepository, GameService, GameController

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -30,6 +30,9 @@ public class Game {
 	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
+    public Game() {
+    }
+	
 	public Game(long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
 			String shortDescription, String longDescription) {
 		this.id = id;
